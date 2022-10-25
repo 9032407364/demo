9206c0ae504cec91b25086e13ba69c513282d6c1
vemuri naveen <vemurinaveen95.vn@gmail.com>
2022-10-25 11:29:55 +0530

    firstCommit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+@GetMapping
+	public String GetValue() {return "hI";}
+	public static void main(String[] args) {
+		//SpringApplication.run(DemoApplication.class, args);
+		
+		ApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
+
+		GreetService greetservice = applicationContext.getBean("greetService",GreetService.class);
+		String message = greetservice.say();
+		System.out.println(message);
+
+	}
+
+}
