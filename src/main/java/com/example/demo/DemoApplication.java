493b9b3c88028e613c7831902085a8ffaad40465
vemuri naveen <vemurinaveen95.vn@gmail.com>
2022-10-25 13:11:23 +0530

    newCommit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 @GetMapping
-	public String GetValue() {return "hI";}
+	public String GetValue() {return "hIEveryone";}
 	public static void main(String[] args) {
 		//SpringApplication.run(DemoApplication.class, args);
 		
